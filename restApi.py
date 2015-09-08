8a42a7b67717d82892a508fd7fd155287f88b28c
andrii333 <andrii.333@mail.ru>
2015-09-08 16:31:22 +0300

    Create restApi.py

---
+++ restApi.py
@@ -0,0 +1,25 @@
+print 'rest API'
+from flask import Flask
+import urllib
+import urllib2
+from flask import request, send_from_directory
+import httplib
+import json
+from werkzeug import secure_filename
+
+
+app = Flask('__name__')
+import os
+
+@app.route("/mystyle_1.css")
+def mystyle_css():
+    idx_f = open('./static/mystyle_1.css','r')
+    idx = idx_f.read()
+    idx_f.close()
+    return idx, 200, {"Content-type":"text/css"}
+
+
+if __name__ == '__main__':
+    app.run(host="172.30.6.42",port=5000)
+
+
